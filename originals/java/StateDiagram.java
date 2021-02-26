5a3c89a9e1c46f2e16c99fbe96fbed63869a89a7
Anders Rundgren <anders.rundgren.net@gmail.com>
2021-02-26 04:14:29 +0100

    Update StateDiagram.java

--- originals/java/StateDiagram.java
+++ originals/java/StateDiagram.java
@@ -6,37 +6,61 @@ import java.io.FileOutputStream;
 
 public class StateDiagram {
 
-    static double VBAR_HEIGHT = 200;
+    static double VBAR_HEIGHT = 700;
     static double VBAR_WIDTH = 2;
     static double VBAR_START = 10;
-    static double VBAR_DISTANCE = 50;
+    static double VBAR_DISTANCE = 150;
     static double VBAR_INITIAL = 150;
 
-    static double PROC_WIDTH = 82;
+    static double PROC_WIDTH = 86;
     static double PROC_SLANT = 8;
     static double PROC_HEIGHT = 22;
+
+    static double NUMB_WIDTH = 16;
+
+    static double STROKE_WIDTH = 0.5;
+
+    static double ARROW_WIDTH = 1;
+    static double ARROW_HEAD_WIDTH = 5;
+    static double ARROW_HEAD_LENGTH = 7;
+    static double ARROW_HEAD_GUTTER = 0.5;
 
     static double DROP_OFFSET = 2;
 
     String originalBase;
     StringBuilder svg = new StringBuilder ("""
 <?xml version='1.0' encoding='utf-8'?>
-<svg width='1000' height='300' xmlns='http://www.w3.org/2000/svg'>
+<svg width='1000' height='1000' xmlns='http://www.w3.org/2000/svg'>
     <title>State Diagram</title>
     <!-- Anders Rundgren 2021 -->
     <defs>
         <filter id='dropShaddow'>
             <feGaussianBlur stdDeviation='1'/>
         </filter>
-    </defs>
 """);
 
+    int currNum = 0;
+    void number(double centerX, double centerY) {
+        svg.append("    <rect x='")
+           .append(convert(centerX - (NUMB_WIDTH + STROKE_WIDTH) / 2))
+           .append("' y='")
+           .append(convert(centerY - (NUMB_WIDTH + STROKE_WIDTH) / 2))
+           .append("' width='")
+           .append(convert(NUMB_WIDTH))
+           .append("' height='")
+           .append(convert(NUMB_WIDTH))
+           .append("' rx='3' fill='white' stroke='black' stroke-width='")
+           .append(convert(STROKE_WIDTH))
+           .append("'/>\n");
+    }
+
     void processing(int vbar, double y) {
+        double rectY = y - (PROC_HEIGHT + STROKE_WIDTH) / 2;
         double startX = center(vbar) - PROC_WIDTH / 2 + PROC_SLANT / 2;
-        svg.append("<path fill='grey' d='M")
+        svg.append("    <path fill='grey' d='M")
            .append(convert(startX + DROP_OFFSET))
            .append(',')
-           .append(convert(DROP_OFFSET + y))
+           .append(convert(DROP_OFFSET + rectY))
            .append(" l")
            .append(convert(PROC_WIDTH))
            .append(',')
@@ -47,10 +71,10 @@ public class StateDiagram {
            .append(" l")
            .append(convert(-PROC_WIDTH))
            .append(",0Z' filter='url(#dropShaddow)'/>\n")
-           .append("<path fill='#fff2cc' stroke='#404040' stroke-width='0.5' d='M")
+           .append("    <path fill='#fff2cc' stroke='#404040' stroke-width='0.5' d='M")
            .append(convert(startX))
            .append(',')
-           .append(convert(y))
+           .append(convert(rectY))
            .append(" l")
            .append(convert(PROC_WIDTH))
            .append(',')
@@ -61,40 +85,74 @@ public class StateDiagram {
            .append(" l")
            .append(convert(-PROC_WIDTH))
            .append(",0Z'/>\n")
-           .append("<text x='")
-           .append(center(vbar))
+           .append("    <text x='")
+           .append(convert(center(vbar) + NUMB_WIDTH / 5))
            .append("' y='")
-           .append(convert(y + 15))
+           .append(convert(rectY + 15))
            .append("' font-family='sans-serif' font-size='12' text-anchor='middle'>Processing</text>\n");
-           /*
-        <path fill='#fff2cc' stroke='#404040' stroke-width='0.5' d='M5,1 l82,0 l-4,22 l-82,0Z'/>
-        <text x='44' y='16' font-family='sans-serif' font-size='12' text-anchor='middle'>Processing</text>
-    </symbol>
-    <use href='#process'/>
-    <use href='#process' x='100' y='50'/>
-    */
+        number(startX - PROC_SLANT / 2 - STROKE_WIDTH / 2, y);
+    }
+
+    void arrow(double centerY, int from, int to) {
+        double startX = (to > from ? VBAR_WIDTH : -VBAR_WIDTH) / 2;
+        double endX = to > from ? -ARROW_HEAD_LENGTH - ARROW_HEAD_GUTTER : ARROW_HEAD_LENGTH + ARROW_HEAD_GUTTER;
+        String y = convert(centerY);
+        svg.append("    <line x1='")
+           .append(convert(center(from) + startX))
+           .append("' y1='")
+           .append(y)
+           .append("' x2='")
+           .append(convert(center(to) + endX - startX))
+           .append("' y2='")
+           .append(y)
+           .append("' stroke-width='")
+           .append(convert(ARROW_WIDTH))
+           .append("' stroke='black' marker-end='url(#arrowHead)'/>\n");
+
     }
 
     double center(int i) {
-        return VBAR_INITIAL - VBAR_WIDTH / 2 + i * VBAR_DISTANCE;
+        return VBAR_INITIAL + i * VBAR_DISTANCE;
     }
 
     StateDiagram(String originalBase) throws Exception {
         this.originalBase = originalBase;
+        svg.append("        <marker id='arrowHead' markerWidth='")
+           .append(convert(ARROW_HEAD_LENGTH))
+           .append("' markerHeight='")
+           .append(convert(ARROW_HEAD_WIDTH))
+           .append("' refX='0' refY='")
+           .append(convert(ARROW_HEAD_WIDTH / 2))
+           .append("' orient='auto'>\n            <polygon points='0 0, ")
+           .append(convert(ARROW_HEAD_LENGTH))
+           .append(" ")
+           .append(convert(ARROW_HEAD_WIDTH / 2))
+           .append(", 0 ")
+           .append(convert(ARROW_HEAD_WIDTH))
+           .append("' />\n" +
+            "        </marker>\n" +
+            "    </defs>\n");
         readOriginal("user.svg");
         for (int i = 0; i < 5; i++) {
-            svg.append("    <rect x='")
-               .append(convert(center(i)))
-               .append("' y='")
+            String x = convert(i * VBAR_DISTANCE + VBAR_INITIAL);
+            svg.append("    <line x1='")
+               .append(x)
+               .append("' y1='")
                .append(convert(VBAR_START))
-               .append("' width='")
+               .append("' x2='")
+               .append(x)
+               .append("' y2='")
+               .append(convert(VBAR_HEIGHT + VBAR_START))
+               .append("' stroke-width='")
                .append(convert(VBAR_WIDTH))
-               .append("' height='")
-               .append(convert(VBAR_HEIGHT))
-               .append("' fill='red'/>\n");
+               .append("' stroke='red'/>\n");
         }
         processing(1, 100);
         processing(2, 150);
+        arrow(150, 3, 4);
+        arrow(170, 3, 4);
+        arrow(190, 4, 3);
+       number(center(3), 150);
         new FileOutputStream(originalBase + "statediagram.svg").write(svg.append("</svg>").toString().getBytes("utf-8"));
      }
 
