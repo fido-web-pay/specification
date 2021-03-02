c92da355df15a778079886b4a5b438c213f0befc
Anders Rundgren <anders.rundgren.net@gmail.com>
2021-03-02 06:37:18 +0100

    more of the same

--- originals/java/StateDiagram.java
+++ originals/java/StateDiagram.java
@@ -8,13 +8,13 @@ import java.io.FileOutputStream;
 public class StateDiagram {
 
     static double ACTOR_TOP_Y = 26;
-    static double ACTOR_HEIGHT = 60;
+    static double ACTOR_HEIGHT = 50;
 
-    static double VBAR_HEIGHT = 700;
+    static double VBAR_HEIGHT = 600;
     static double VBAR_WIDTH = 2;
     static double VBAR_START_Y = ACTOR_TOP_Y + ACTOR_HEIGHT - 5;
-    static double VBAR_DISTANCE = 150;
-    static double VBAR_START_X = 150;
+    static double VBAR_X_DISTANCE = 120;
+    static double VBAR_START_X = ACTOR_HEIGHT / 1.8;
     static String VBAR_COLOR = "#cc2020";
 
     static double PROC_WIDTH = 86;
@@ -37,9 +37,19 @@ public class StateDiagram {
 
     static double HEADER_Y = FONT_SIZE;
 
-    static String CLICKABLE_COLOR = "blue";
+    static double UI_WIDTH = 86;
+    static double UI_HEIGHT = 50;
+    static double UI_BORDER = 5;
  
+    static String CLICKABLE_COLOR = "blue";
+
+    static double SEQ_Y_DISTANCE = 32;
+    static double SEQ_Y_SLACK_AFTER_UI = 0;
+
     String originalBase;
+
+    double seqY = 110;
+
     StringBuilder svg = new StringBuilder ("""
 <?xml version='1.0' encoding='utf-8'?>
 <svg width='1000' height='1000' xmlns='http://www.w3.org/2000/svg'>
@@ -109,8 +119,8 @@ public class StateDiagram {
            .append("</text>\n");
     }
 
-    void processing(int vbar, double y, String url) {
-        double rectY = y - (PROC_HEIGHT + STROKE_WIDTH) / 2;
+    void processing(int vbar, String url) {
+        double rectY = seqY - (PROC_HEIGHT + STROKE_WIDTH) / 2;
         double startX = center(vbar) - PROC_WIDTH / 2 + PROC_SLANT / 2;
         svg.append("  <path fill='black' opacity='0.4' d='M")
            .append(convert(startX + DROP_OFFSET))
@@ -140,22 +150,23 @@ public class StateDiagram {
            .append(" l")
            .append(convert(-PROC_WIDTH))
            .append(",0Z'/>\n");
-        text(center(vbar) + NUMB_WIDTH / 5, y, "Processing");
-        number(startX - PROC_SLANT / 2 - STROKE_WIDTH / 2, y, url);
+        text(center(vbar) + NUMB_WIDTH / 5, seqY, "Processing");
+        number(startX - PROC_SLANT / 2 - STROKE_WIDTH / 2, seqY, url);
+        seqY += SEQ_Y_DISTANCE + SEQ_Y_SLACK_AFTER_UI;
     }
 
-    void dashedArrow(double centerY, int from, int to, String label, String url) {
-        _arrow(centerY, from, to, label, " stroke-dasharray='4'", url);
+    void dashedArrow(int from, int to, String label, String url) {
+        _arrow(from, to, label, " stroke-dasharray='4'", url);
     }
 
-    void arrow(double centerY, int from, int to, String label, String url) {
-        _arrow(centerY, from, to, label, "", url);
+    void arrow(int from, int to, String label, String url) {
+        _arrow(from, to, label, "", url);
     }
 
-    void _arrow(double centerY, int from, int to, String label, String additional, String url) {
+    void _arrow(int from, int to, String label, String additional, String url) {
         double startX = (to > from ? VBAR_WIDTH : -VBAR_WIDTH) / 2;
         double endX = to > from ? -ARROW_HEAD_LENGTH - ARROW_HEAD_GUTTER : ARROW_HEAD_LENGTH + ARROW_HEAD_GUTTER;
-        String arrowY = convert(centerY);
+        String arrowY = convert(seqY);
         double textX = (center(to) + center(from)) / 2;
         svg.append("  <line x1='")
            .append(convert(center(from) + startX))
@@ -170,12 +181,13 @@ public class StateDiagram {
            .append("' stroke='black' marker-end='url(#arrowHead)'")
            .append(additional)
            .append("/>\n");
-        text(textX, centerY - FONT_SIZE * 0.8, label);
-        number(center(from), centerY, url);
+        text(textX, seqY - FONT_SIZE * 0.8, label);
+        number(center(from), seqY, url);
+        seqY += SEQ_Y_DISTANCE;
     }
 
     double center(int i) {
-        return VBAR_START_X + i * VBAR_DISTANCE;
+        return VBAR_START_X + i * VBAR_X_DISTANCE;
     }
 
     String embedded;
@@ -237,7 +249,7 @@ public class StateDiagram {
             "    </marker>\n" +
             "  </defs>\n");
         for (int i = 0; i < 5; i++) {
-            String x = convert(i * VBAR_DISTANCE + VBAR_START_X);
+            String x = convert(i * VBAR_X_DISTANCE + VBAR_START_X);
             svg.append("  <line x1='")
                .append(x)
                .append("' y1='")
@@ -250,59 +262,64 @@ public class StateDiagram {
                .append(convert(VBAR_WIDTH))
                .append("' stroke='" + VBAR_COLOR + "'/>\n");
         }
-        embedded = readOriginal("user.svg")
-           .replace("height='128'\n   width='128'", "viewBox='0 0 128 128'");
-        svg.append("<svg x='")
-           .append(convert(center(0) - ACTOR_HEIGHT / 2))
-           .append("' y='")
-           .append(ACTOR_TOP_Y)
-           .append("' width='")
-           .append(ACTOR_HEIGHT)
-           .append("' height='")
-           .append(ACTOR_HEIGHT)
-           .append("'")
-           .append(embedded.substring(embedded.indexOf("<svg") + 4));
-/*
-        embedded = readOriginal("browser.svg")
-           .replace("width='72' height='72'", "viewBox='0 0 72 72'");
-        svg.append("<svg x='")
-           .append(convert(center(1) - ACTOR_HEIGHT / 2))
-           .append("' y='")
-           .append(ACTOR_TOP_Y)
-           .append("' width='")
-           .append(ACTOR_HEIGHT)
-           .append("' height='")
-           .append(ACTOR_HEIGHT)
-           .append("'")
-          .append(embedded.substring(embedded.indexOf("<svg") + 4));
-
-        double embeddedWidth = ACTOR_HEIGHT * 68 / 48;
-        embedded = readOriginal("merchant.svg")
-           .replace("width='68'\n   height='48'", "viewBox='0 0 68 48'");
-        svg.append("<svg x='")
-           .append(convert(center(2) - embeddedWidth / 2))
-           .append("' y='")
-           .append(ACTOR_TOP_Y)
-           .append("' width='")
-           .append(convert(embeddedWidth))
-           .append("' height='")
-           .append(ACTOR_HEIGHT)
-           .append("'")
-          .append(embedded.substring(embedded.indexOf("<svg") + 4));
-*/
 
         actor("user.svg",     "User",     0);
         actor("browser.svg",  "Browser",  1);
         actor("merchant.svg", "Merchant", 2);
         actor("acquirer.svg", "Acquirer", 3);
         actor("issuer.svg",   "Issuer",   4);
-  
-        processing(1, 200, "#init");
-        processing(2, 150, "#browserui");
-        arrow(150, 3, 4, "KURT", "#kurt");
-        arrow(170, 3, 4, "Yes this!", "#something");
-        arrow(190, 4, 3, "blah", "#else");
-        dashedArrow(210, 3, 4, "Auth", "#userauth");
+
+        arrow(2, 1, "PREQ", "#preq");
+
+        double uiY = seqY + (UI_HEIGHT - PROC_HEIGHT) / 2;
+        double uiTop = uiY - (UI_HEIGHT + STROKE_WIDTH) / 2;
+        double uiX = center(1) - (UI_WIDTH + STROKE_WIDTH) / 2;
+        svg.append("  <rect x='")
+           .append(convert(uiX + DROP_OFFSET))
+           .append("' y='")
+           .append(convert(uiTop + DROP_OFFSET))
+           .append("' width='")
+           .append(convert(UI_WIDTH))
+           .append("' height='")
+           .append(convert(UI_HEIGHT))
+           .append("' fill='black' opacity='0.4' filter='url(#dropShaddow)'/>\n")
+           .append("  <rect x='")
+           .append(convert(uiX))
+           .append("' y='")
+           .append(convert(uiTop))
+           .append("' width='")
+           .append(convert(UI_WIDTH))
+           .append("' height='")
+           .append(convert(UI_HEIGHT))
+           .append("' fill='white' stroke-width='")
+           .append(convert(STROKE_WIDTH))
+           .append("' stroke='black'/>\n")
+           .append("  <rect x='")
+           .append(convert(uiX))
+           .append("' y='")
+           .append(convert(uiTop))
+           .append("' width='")
+           .append(convert(UI_WIDTH))
+           .append("' height='")
+           .append(convert(UI_BORDER))
+           .append("' fill='#deeafc' stroke-width='")
+           .append(convert(STROKE_WIDTH))
+           .append("' stroke='black'/>\n");  
+        text(center(1), uiY, "Payment UI");
+        number(uiX, uiTop + (UI_BORDER + STROKE_WIDTH) / 2, "#paymentui");
+        seqY += SEQ_Y_DISTANCE + SEQ_Y_SLACK_AFTER_UI + UI_HEIGHT - PROC_HEIGHT;
+        dashedArrow(0, 1, "Authorization", "#userauth");
+        processing(1, "#init");
+        arrow(1, 2, "PRES", "#pres");
+        processing(2, "#merchant");
+        arrow(2, 3, "MA-REQ", "#ma-req");
+        processing(3, "#acquirer");
+        arrow(3, 4, "AI-REQ", "#ai-req");
+        processing(4, "#issuer");
+        dashedArrow(4, 3, "TBD", "#tbd");
+ //      arrow(170, 3, 4, "Yes this!", "#something");
+  //      arrow(190, 4, 3, "blah", "#else");
+  //      dashedArrow(210, 3, 4, "Auth", "#userauth");
        new FileOutputStream(originalBase + "statediagram.svg").write(svg.append("</svg>").toString().getBytes("utf-8"));
      }
 
